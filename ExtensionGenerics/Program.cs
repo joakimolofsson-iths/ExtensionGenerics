a9b9eaef50c42bc60cfe491033f07d62c0f8d716
Joakim Olofsson <joakim.olofsson@iths.se>
2024-10-08 14:48:23 +0200

    Adding project files

---
+++ ExtensionGenerics/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExtensionGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
