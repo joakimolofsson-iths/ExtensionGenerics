65bfb7b9dd2f521a51987662f4d026bb84e23d2e
Joakim Olofsson <joakim.olofsson@iths.se>
2024-10-08 15:00:03 +0200

    Extension exercise done

--- ExtensionGenerics/Program.cs
+++ ExtensionGenerics/Program.cs
@@ -4,7 +4,49 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            IVehicle myCar = new Car("Volvo", "V70");
+            IVehicle myBoat = new Boat("Cornwall", "Endurance");
+
+            myCar.ShowVehicle();
+            myBoat.ShowVehicle();
+        }
+    }
+
+    interface IVehicle
+    {
+        string Manufacturer { get; }
+        string Model { get; }
+
+    }
+    class Car : IVehicle
+    {
+        public string Manufacturer { get; private set; }
+        public string Model { get; private set; }
+
+        public Car(string manufacturer, string model)
+        {
+            Manufacturer = manufacturer;
+            Model = model;
+        }
+    }
+
+    class Boat : IVehicle
+    {
+        public string Manufacturer { get; private set; }
+        public string Model { get; private set; }
+
+        public Boat(string manufacturer, string model)
+        {
+            Manufacturer = manufacturer;
+            Model = model;
+        }
+    }
+
+    static class VehicleExtension
+    {
+        public static void ShowVehicle(this IVehicle vehicle)
+        {
+            Console.WriteLine($"{vehicle.Manufacturer} {vehicle.Model}");
         }
     }
 }
